b623dc638395fda8c0bb94e94351c382d59673ab
shuo <shuo.li88@hotmail.com>
2020-10-13 09:21:22 +0800

    update some styles

--- public/script.js
+++ public/script.js
@@ -1,48 +1,49 @@
-//input.style.opacity = 0;
-
-// 1. get the  file name of first uploaded file--model file
+// name a few variables
 
 const inputModelFile = document.querySelector("#modelfile");
 const modelmessage = document.querySelector("#modelmessage");
 const inputBlastFile = document.querySelector("#blastfile");
 const blastmessage = document.querySelector("#blastmessage");
 inputModelFile.addEventListener("change", updateModelFile);
-// inputModelFile.addEventListener("click", modelAlert);
 inputBlastFile.addEventListener("change", updateBlastFile);
 
+// function to get the uploaded model file name:
 
-
-// function to get the uploaded file name:
-
-function getFirstFileName() {
+function getModelFileName() {
   const file = inputModelFile.files;
   //console.log(file[0]);
   const nameArray = file[0].name.split(".");
   //console.log(nameArray);
-  const firstFileName = nameArray[0];
-  return firstFileName;
+  const modelFileName = nameArray[0];
+  return modelFileName;
 }
 
+// function to get the blast file name
 
-function getSecondFileName() {
+function getBlastFileName() {
   const file = inputBlastFile.files;
   //console.log(file[0]);
   const nameArray = file[0].name.split(".");
   //console.log(nameArray);
-  const secondFileName = nameArray[0];
-  return secondFileName;
+  const blastFileName = nameArray[0];
+  return blastFileName;
 }
 
+// function to get the uploaded model file type
 
-// function to get the uploaded file type
-
-function getFileType() {
+function getModelFileType() {
   const file = inputModelFile.files;
-  const fileType = file[0].name.split(".").pop();
-  return fileType;
+  const fileTypeModel = file[0].name.split(".").pop();
+  return fileTypeModel;
 }
 
+// function to get the uploaded model file type
 
+function getBlastFileType() {
+  const file = inputBlastFile.files;
+  const fileTypeBlast = file[0].name.split(".").pop();
+  return fileTypeBlast;
+}
 
 // Function to upload the model file
 
@@ -52,24 +53,15 @@ function updateModelFile() {
     modelmessage.removeChild(modelmessage.firstChild);
   }
 
-  const fileName = getFirstFileName();
-  //console.log(fileName);
-
-  const fileType = getFileType();
-  //console.log(fileType);
-
-  // const curFile1 = inputModelFile.files;
-  // console.log(curFile1[0]);
-  // const nameArray = curFile1[0].name.split('.')
-  // console.log(nameArray)
-  // const fileType= curFile1[0].name.split('.').pop()
+  const modelFileName = getModelFileName();
+  const fileTypeModel = getModelFileType();
 
   const para = document.createElement("p");
 
   // if filename is empty
-  if (!fileName) {
+  if (!modelFileName) {
     para.textContent = "No files currently selected for upload";
-  } else if (fileType !== "csv") {
+  } else if (fileTypeModel !== "csv") {
     // if file type is not csv, pop up message of incorrect file type
     para.textContent = "File type is not correct, please upload a csv file!";
     // message of sweet alert
@@ -78,20 +70,20 @@ function updateModelFile() {
       title: "Oops...",
       text: "File type is wrong!",
     });
-  } else if (fileName === "block_model") {
+  } else if (modelFileName === "block_model") {
     // if the file name matches block_model, showing the message of successful upload
-    console.log(fileName);
-    para.textContent = `File name ${fileName} successfully uploaded! `;
+    console.log(modelFileName);
+    para.textContent = `File name ${modelFileName} successfully uploaded! `;
 
     //message of sweet alert
     Swal.fire({
       icon: "success",
       title: "Great!",
-      text: "File uploaded successfully!",
+      text: "Your model file is uploaded successfully!",
     });
   } else {
     // if file name doesn't match, show the message of file name incorrect
-    para.textContent = `File name ${fileName} . Please upload the correct file!`;
+    para.textContent = `File name ${modelFileName} . Please upload the correct file!`;
     // sweet alert message
     Swal.fire({
       icon: "error",
@@ -104,36 +96,44 @@ function updateModelFile() {
 }
 
 
-
-
+// FUNCTION OF UPLOADING BLAST FILE
 function updateBlastFile() {
   // clear the previous file name
   while (blastmessage.firstChild) {
     blastmessage.removeChild(blastmessage.firstChild);
   }
 
-  const fileName2 = getSecondFileName();
-  //console.log(fileName);
-
-  const fileType = getFileType();
-  //console.log(fileType);
-
-  // const curFile = inputBlastFile.files;
-  // console.log(curFile[0]);
-  // const nameArray = curFile[0].name.split(".");
-  // console.log(nameArray);
-  // const fileType = curFile[0].name.split(".").pop();
-
+  const blastFileName = getBlastFileName();
+  const fileTypeBlast = getBlastFileType();
   const para = document.createElement("p");
 
-  if (!fileName2) {
+  if (!blastFileName) {
     para.textContent = "No files currently selected for upload";
-  } else if (fileType !== "csv") {
+  } else if (fileTypeBlast !== "csv") {
     para.textContent = "File type is not correct, please upload a csv file!";
-  } else if (nameArray[0] !== "Blast_design") {
-    para.textContent = "Please upload the correct file!";
+    //message of sweet alert
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "File type is wrong!",
+    });
+  } else if (blastFileName !== "Blast_design") {
+    para.textContent = `File name ${blastFileName} . Please upload the correct file!`;
+    //message of sweet alert
+    Swal.fire({
+      icon: "error",
+      title: "File name is not right",
+      text: "Please upload the correct file!",
+    });
   } else {
-    para.textContent = `File name ${curFile[0].name} successfully uploaded !`;
+    para.textContent = `File name ${blastFileName} successfully uploaded !`;
+
+    //message of sweet alert
+    Swal.fire({
+      icon: "success",
+      title: "Great!",
+      text: "Your blast file is uploaded successfully!",
+    });
   }
   blastmessage.appendChild(para);
 }
@@ -141,30 +141,28 @@ function updateBlastFile() {
 // run terminal after clicking "run program" button
 // if the files are not correct, the program cannot run
 function runProgram() {
-  const file1 = getFirstFileName();
+  const file1 = getModelFileName();
   console.log(file1);
- const file2 = getSecondFileName();
- console.log(file2);
+  const file2 = getBlastFileName();
+  console.log(file2);
+  // both file names need to match to show the correct message
+  if (file1 == "block_model" && file2 == "Blast_design") {
+    console.log("both files are correct!");
+    //message of sweet alert
+    Swal.fire({
+      icon: "success",
+      title: "Great!",
+      text: "File executed successfully in terminal !",
+    });
 
- if (file1 == "block_model" && file2 == "Blast_design"){
-   console.log("both files are correct!")
-
-   Swal.fire({
-    icon: "success",
-    title: "Great!",
-    text: "File executed successfully in terminal !",
-  })
-
-
- }else {
-
-  Swal.fire({
-    icon: "error",
-    title: "something is wrong",
-    text: "Please make sure you upload the correct files !",
-  });
-
- }
-  
-  
+    window.location.href = '/run';
+  } else {
+    console.log("something is wrong !");
+    //message of sweet alert
+    Swal.fire({
+      icon: "error",
+      title: "something is wrong",
+      text: "Please make sure you upload the correct files !",
+    });
+  }
 }
